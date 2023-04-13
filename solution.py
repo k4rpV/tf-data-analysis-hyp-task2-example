9d2697ccbd100c6bbd1f3b2eabfa09f637d76a9a
k4rpV <101994618+k4rpV@users.noreply.github.com>
2023-04-13 19:21:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,4 +4,4 @@ import numpy as np
 chat_id = 953761952
 
 def solution(x: np.array, y: np.array) -> bool:
-    return cramervonmises_2samp(x, y).pvalue > 0.08
+    return cramervonmises_2samp(x, y).pvalue < 0.08
