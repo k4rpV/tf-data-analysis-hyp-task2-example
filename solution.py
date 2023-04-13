a14395be5226e94e79153b43078567d9ae1f8016
k4rpV <101994618+k4rpV@users.noreply.github.com>
2023-04-13 18:42:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,4 +4,4 @@ import numpy as np
 chat_id = 953761952
 
 def solution(x: np.array, y: np.array) -> bool:
-    return anderson_ksamp([x, y]).pvalue > 0.08
+    return anderson_ksamp([x, y]).pvalue < 0.08
